a9542c33221eaafc9f10f4c985b7e596458ca656
ianpatricck <ianpatrick499@gmail.com>
2025-03-19 17:18:36 -0300

    feat: axios http client was initialized

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Definindo a BASE_URL
+const api = axios.create({
+  baseURL: "https://rickandmortyapi.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Função para pegar dados da API
+export const fetchData = async (endpoint: string) => {
+  try {
+    const response = await api.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+    throw error;
+  }
+};
