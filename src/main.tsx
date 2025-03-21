a87d683e2e4384ef4315f0616471abc4fce89580
ianpatricck <ianpatrick499@gmail.com>
2025-03-20 22:32:22 -0300

    feat: App component wrapped by react-router and chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import "./reset.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>,
 );
