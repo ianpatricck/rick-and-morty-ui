1b8b3599e7be483c8926c5d27e7bb850b2460ce0
ianpatricck <ianpatrick499@gmail.com>
2025-03-21 18:51:10 -0300

    feat: request the first characters from API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
 import SearchInput from "@/components/SearchInput";
 import Content from "./layouts/Content";
-import { Flex, SimpleGrid } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
 import CharacterCard from "@/components/CharacterCard";
 import Pagination from "@/components/Pagination";
+import { useQuery } from "@tanstack/react-query";
+import { fetchData } from "@/services/api";
+import { CharacterSchema } from "@/schemas/CharacterSchema";
 
 // Página principal onde se encontra grande parte
 // do conteúdo da aplicação
 export default function Home() {
+  // Utiliza o react-query para fazer uma requição à API.
+  const {
+    isPending,
+    isError,
+    data: characters,
+  } = useQuery({
+    queryKey: ["characters"],
+    queryFn: () => fetchData("/character?page=1"),
+  });
+
+  // Exibe uma mensagem de erro no console.
+  if (isError) {
+    console.error(isError);
+  }
+
   return (
     <Flex
       direction="column"
@@ -20,90 +38,33 @@ export default function Home() {
         </Flex>
 
         <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="5px">
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-            name="Rick Sanchez"
-            status="Alive"
-            species="Human"
-            gender="Male"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-            name="Morty Smith"
-            status="Alive"
-            species="Human"
-            gender="Male"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-            name="Summer Smith"
-            status="Alive"
-            species="Human"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
-            name="Beth Smith"
-            status="Alive"
-            species="Human"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
-            name="Jerry Smith"
-            status="Alive"
-            species="Human"
-            gender="Male"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
-          <CharacterCard
-            image="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
-            name="Abadango Cluster Princess"
-            status="Alive"
-            species="Alien"
-            gender="Female"
-          />
+          {/*Verifica se a requisição ainda está pendente. Caso sim, mostra o Skeleton Loading.*/}
+          {isPending ? (
+            <>
+              {Array.apply(null, Array(20)).map(
+                (_item: unknown, key: number) => (
+                  <Stack gap="6" maxW="xs" key={key}>
+                    <Skeleton height="200px" />
+                  </Stack>
+                ),
+              )}
+            </>
+          ) : (
+            <>
+              {characters?.results.map(
+                (character: CharacterSchema, key: number) => (
+                  <CharacterCard
+                    image={character.image}
+                    name={character.name}
+                    status={character.status}
+                    species={character.species}
+                    gender={character.gender}
+                    key={key}
+                  />
+                ),
+              )}
+            </>
+          )}
         </SimpleGrid>
       </Content>
 
