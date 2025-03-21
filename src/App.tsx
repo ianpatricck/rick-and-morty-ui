3275a19876b8ab1586080662878ef9be595b02e3
ianpatricck <ianpatrick499@gmail.com>
2025-03-20 22:30:58 -0300

    feat: implemented react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "@/pages/Home";
+import PageNotFound from "@/pages/PageNotFound";
+
 export default function App() {
   return (
-    <div>
-      <h1>Rick And Morty UI</h1>
-    </div>
+    <>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
