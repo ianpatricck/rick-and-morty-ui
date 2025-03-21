b608e03b505232a524907646c9b3d14c175406db
ianpatricck <ianpatrick499@gmail.com>
2025-03-21 16:08:43 -0300

    feat: created a view character route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router";
 import Home from "@/pages/Home";
 import PageNotFound from "@/pages/PageNotFound";
+import Character from "@/pages/Character";
 
+// Entrypoint do projeto, o componente principal.
 export default function App() {
   return (
     <>
       <Routes>
         <Route index path="/" element={<Home />} />
+        <Route index path="/character/:id" element={<Character />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
