0d4e741ae7fc7c12bab8250b2c64283e49d30b44
ianpatricck <ianpatrick499@gmail.com>
2025-03-21 18:50:21 -0300

    feat: implemented the QueryClientProvider as a wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import "./reset.css";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
+
+// Renderizando todo o documento
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>,
 );
