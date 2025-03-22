b73f12ae78671de122bd7b5fccb12cb681ff291e
ianpatricck <ianpatrick499@gmail.com>
2025-03-22 10:48:18 -0300

    fix(pagination): fixed the search for characters dynamically

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,30 @@
 import SearchInput from "@/components/SearchInput";
 import Content from "./layouts/Content";
-import { Flex, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
+import { Flex, Heading, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
 import CharacterCard from "@/components/CharacterCard";
 import Pagination from "@/components/Pagination";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { fetchData } from "@/services/api";
 import { CharacterSchema } from "@/schemas/CharacterSchema";
 import { Link } from "react-router";
+import { useState } from "react";
 
 // Página principal onde se encontra grande parte
 // do conteúdo da aplicação
 export default function Home() {
-  // Utiliza o react-query para fazer uma requição à API.
+  const [page, setPage] = useState<number>(1);
+
+  // Utiliza o react-query para fazer uma requisição à API.
   const {
     isPending,
     isError,
     data: characters,
+    isFetching,
   } = useQuery({
-    queryKey: ["characters"],
-    queryFn: () => fetchData("/character?page=1"),
+    queryKey: ["characters", page],
+    queryFn: () => fetchData("/character?page=" + page),
+    placeholderData: keepPreviousData,
   });
-
-  // Exibe uma mensagem de erro no console.
-  if (isError) {
-    console.error(isError);
-  }
 
   return (
     <Flex
@@ -33,6 +33,7 @@ export default function Home() {
       backgroundColor="blue"
       padding={10}
     >
+      {/*Armazena todo o conteúdo da Home*/}
       <Content>
         <Flex backgroundColor="green" justifyContent="center" padding={2}>
           <SearchInput />
@@ -40,7 +41,7 @@ export default function Home() {
 
         <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="5px">
           {/*Verifica se a requisição ainda está pendente. Caso sim, mostra o Skeleton Loading.*/}
-          {isPending ? (
+          {isPending || isFetching ? (
             <>
               {Array.apply(null, Array(20)).map(
                 (_item: unknown, key: number) => (
@@ -50,6 +51,10 @@ export default function Home() {
                 ),
               )}
             </>
+          ) : isError ? (
+            <Heading textAlign="center" margin={4}>
+              Erro ao buscar parsonagens
+            </Heading>
           ) : (
             <>
               {characters?.results.map(
@@ -70,7 +75,24 @@ export default function Home() {
         </SimpleGrid>
       </Content>
 
-      <Pagination />
+      {/*Exibe o componente de páginação caso isError seja 'false'*/}
+
+      {!!isPending ? (
+        <Skeleton height="40px" width="40vw" mt={4} />
+      ) : (
+        !isError && (
+          <Pagination
+            count={characters?.info.pages}
+            pageSize={1}
+            page={page}
+            defaultPage={1}
+            onPageChange={(e) => {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+              setPage(e.page);
+            }}
+          />
+        )
+      )}
     </Flex>
   );
 }
