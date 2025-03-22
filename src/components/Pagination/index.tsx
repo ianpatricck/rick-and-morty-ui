bb057487ca30866b598eeec98ceb190a2415a13c
ianpatricck <ianpatrick499@gmail.com>
2025-03-22 10:45:59 -0300

    feat: implemented a compact pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,35 +1,37 @@
+import { useWindowScreen } from "@/hooks/useWindowScreen";
 import {
   ButtonGroup,
   Pagination as ChakraPagination,
   IconButton,
+  PaginationRootProps,
 } from "@chakra-ui/react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
 // Componente de paginação que fornece usabilidade ao usuário
 // ao navegar pelos itens dos personagens.
-export default function Pagination() {
+export default function Pagination(props: PaginationRootProps) {
+  const screenSize = useWindowScreen();
+
   return (
-    <ChakraPagination.Root
-      count={20}
-      pageSize={2}
-      defaultPage={1}
-      marginTop={2}
-      backgroundColor="dimgray"
-    >
-      <ButtonGroup variant="ghost" size="sm">
+    <ChakraPagination.Root marginTop={2} backgroundColor="dimgray" {...props}>
+      <ButtonGroup variant="ghost" size={{ base: "xs", sm: "sm", md: "md" }}>
         <ChakraPagination.PrevTrigger asChild>
           <IconButton>
             <LuChevronLeft />
           </IconButton>
         </ChakraPagination.PrevTrigger>
 
-        <ChakraPagination.Items
-          render={(page) => (
-            <IconButton variant={{ base: "ghost", _selected: "outline" }}>
-              {page.value}
-            </IconButton>
-          )}
-        />
+        {screenSize.width < 385 ? (
+          <ChakraPagination.PageText />
+        ) : (
+          <ChakraPagination.Items
+            render={(page) => (
+              <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                {page.value}
+              </IconButton>
+            )}
+          />
+        )}
 
         <ChakraPagination.NextTrigger asChild>
           <IconButton>
