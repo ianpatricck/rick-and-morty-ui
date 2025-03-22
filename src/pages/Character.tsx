88e35ee3b9416b0039100901777c50883d5005a1
ianpatricck <ianpatrick499@gmail.com>
2025-03-21 23:08:06 -0300

    feat(pages/Character): created a request to a specific character and showing it

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,9 +1,28 @@
+import { useParams } from "react-router";
 import Content from "./layouts/Content";
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Skeleton, Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchData } from "@/services/api";
 
-// Componente que exibe mais informações
-// de um usuário em específico.
+// Componente que exibe mais informações de um personagem em específico.
 export default function Character() {
+  const { id } = useParams();
+
+  // Utiliza o react-query para fazer a requição do personagem na API.
+  const {
+    isPending,
+    isError,
+    data: character,
+  } = useQuery({
+    queryKey: ["character"],
+    queryFn: () => fetchData("/character/" + id),
+  });
+
+  // Exibe uma mensagem de erro no console.
+  if (isError) {
+    console.error(isError);
+  }
+
   return (
     <Flex justifyContent="center" padding={25} backgroundColor="blue" h="100vh">
       <Content>
@@ -13,30 +32,49 @@ export default function Character() {
           backgroundColor="green"
           rounded="lg"
         >
-          <Flex
-            backgroundColor="brown"
-            justifyContent="center"
-            padding={{ base: 2, sm: 2, md: 0 }}
-          >
-            <Image
-              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-              alt="Rick Sanchez"
-              height={{ base: "150px", sm: "200px", md: "300px", lg: "350px" }}
-              width={{ base: "150px", sm: "200px", md: "300px" }}
-              rounded={{ base: "full" }}
-              roundedTopRight={{ md: "none" }}
-              roundedBottomRight={{ md: "none" }}
-              roundedBottomLeft={{ md: "lg" }}
-              roundedTopLeft={{ md: "lg" }}
+          {isPending ? (
+            <Skeleton
+              height={{
+                base: "150px",
+                sm: "200px",
+                md: "300px",
+                lg: "350px",
+              }}
+              width="100%"
             />
-          </Flex>
+          ) : (
+            <>
+              <Flex
+                backgroundColor="brown"
+                justifyContent="center"
+                padding={{ base: 2, sm: 2, md: 0 }}
+              >
+                <Image
+                  src={character.image}
+                  alt={character.name}
+                  height={{
+                    base: "150px",
+                    sm: "200px",
+                    md: "300px",
+                    lg: "350px",
+                  }}
+                  width={{ base: "150px", sm: "200px", md: "300px" }}
+                  rounded={{ base: "full" }}
+                  roundedTopRight={{ md: "none" }}
+                  roundedBottomRight={{ md: "none" }}
+                  roundedBottomLeft={{ md: "lg" }}
+                  roundedTopLeft={{ md: "lg" }}
+                />
+              </Flex>
 
-          <Box margin={2}>
-            <Heading>Name: Rick Sanchez</Heading>
-            <Text>Status: Alive</Text>
-            <Text>Epsodes: 1230</Text>
-            <Text>Location: Earth</Text>
-          </Box>
+              <Box margin={2}>
+                <Heading>Name: {character.name}</Heading>
+                <Text>Status: {character.status}</Text>
+                <Text>Epsodes: {character.episode.length}</Text>
+                <Text>Location: {character.location.name}</Text>
+              </Box>
+            </>
+          )}
         </Flex>
       </Content>
     </Flex>
