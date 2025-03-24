02984f53543e6f445bbede83230659e516af86d3
ianpatricck <ianpatrick499@gmail.com>
2025-03-24 16:01:48 -0300

    refactor: SearchInput component was styled

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,16 +1,20 @@
 import { defineStyle, Field, Group, Input, InputProps } from "@chakra-ui/react";
+import SearchInputModule from "./SearchInput.module.css";
 
 // Campo de busca dos personagens
 export default function SearchInput(props: InputProps) {
   return (
-    <Field.Root w="2xl" backgroundColor="pink">
-      <Group attached w="full">
+    <Field.Root w="2xl">
+      <Group attached w="full" className={SearchInputModule.input_group}>
         <Input
           className="peer"
           placeholder=""
           {...props}
         />
-        <Field.Label css={floatingStyles} fontSize={{ base: "10pt", sm: "sm", md: "md" }}>
+        <Field.Label
+          css={floatingStyles}
+          fontSize={{ base: "10pt", sm: "sm", md: "md" }}
+        >
           Search characters
         </Field.Label>
       </Group>
@@ -21,20 +25,16 @@ export default function SearchInput(props: InputProps) {
 // Estilização para o efeito 'Floating Label'
 const floatingStyles = defineStyle({
   pos: "absolute",
-  bg: "bg",
   px: "0.5",
   top: "-3",
   insetStart: "2",
-  fontWeight: "normal",
   pointerEvents: "none",
   transition: "position",
   _peerPlaceholderShown: {
-    color: "fg.muted",
     top: "2.5",
     insetStart: "3",
   },
   _peerFocusVisible: {
-    color: "fg",
     top: "-5",
     insetStart: "2",
   },
