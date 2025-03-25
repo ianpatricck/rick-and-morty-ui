c62aae59b53b167aeb82e53200d7492b628c6ac5
ianpatricck <ianpatrick499@gmail.com>
2025-03-24 21:38:09 -0300

    fix(formatSearchCharactersQueryD): removed array joins

--- src/utils/formatSearchCharactersQuery.ts
+++ src/utils/formatSearchCharactersQuery.ts
@@ -2,8 +2,8 @@ type SearchCharactersQueryParamsType = {
   path: string;
   page: number;
   character?: string;
-  status?: string[];
-  gender?: string[];
+  status?: string | null;
+  gender?: string | null;
 };
 
 export function formatSearchCharactersQuery(
@@ -11,8 +11,8 @@ export function formatSearchCharactersQuery(
 ): string {
   var query = `${options.path}?page=${options.page}`;
   query += options.character ? "&name=" + options.character : "";
-  query += options.status?.length ? "&status=" + options.status?.join(",") : "";
-  query += options.gender?.length ? "&gender=" + options.gender?.join(",") : "";
+  query += options.status?.length ? "&status=" + options.status : "";
+  query += options.gender?.length ? "&gender=" + options.gender : "";
 
   return query;
 }
