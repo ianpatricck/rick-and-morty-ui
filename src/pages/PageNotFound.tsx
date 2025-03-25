69c41a957e87ea86a1c97f2aa99adfacaf1d91df
ianpatricck <ianpatrick499@gmail.com>
2025-03-24 22:42:48 -0300

    style: styled the NotFound page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,11 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-    </div>
+    <Flex justifyContent="center" alignItems="center" height="80vh">
+      <Heading size={{ sm: "md", md: "xl", xl: "2xl" }} color="dimgray">
+        404 | Content Not Found
+      </Heading>
+    </Flex>
   );
 }
